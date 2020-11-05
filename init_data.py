b4538b16c2cb22f6565de70790d93b3d3ba710e4
Bracha-Eisenstein <57355251+Bracha-Eisenstein@users.noreply.github.com>
2020-11-05 15:53:55 +0200

    Add files via upload

--- init_data.py
+++ init_data.py
@@ -1,61 +1,40 @@
 import json
-
 trie = {}
-sentences_data = {}
-
-
-def add_sub(sub, id, offset):
-    tmp_trie = trie
-    for letter in sub:
-        if letter not in tmp_trie.keys():
-            tmp_trie[letter] = {}
-            tmp_trie[letter]['sentences_ids'] = [{'id': id, 'offset': offset}]
-        else:
-            tmp_trie[letter]['sentences_ids'].append({'id': id, 'offset': offset})
-        tmp_trie = tmp_trie[letter]
-
-
-def add_sentence_to_trie(sentence, id):
-    for index in range(len(sentence)+1):
-        add_sub(sentence[index:], id, index)
-
-
-def add_data_to_sentences_data(sentence, index, source):
-    sentences_data[index] = {'sentence': sentence, 'source': source}
-
+sentences ={}
 
 def get_data_from_file(file_name):
     the_file = open(file_name)
     return the_file.read().split("\n")
 
+def add_sentence_to_data(sentence,index,source):
+    sentences[index] = {"sentence": sentence, "source": source}
 
-# def add_to_json(data_to_insert):
-#     with open("data.json", "w") as data_file:
-#         json.dump(data_to_insert, data_file)
-#
-#
-# def insert_data(file_name):
-#     data = get_data_from_file(file_name)
-#     for index,sentence in enumerate(data):
-#         add_sentence_to_trie(sentence.lower(),index)
-#         add_data_to_sentences_data(sentence, index, file_name)
-#
-#
-#
-# def init():
-#     insert_data("data.txt")
-#     insert_data("freedom.txt")
-#     add_to_json({'sentences': sentences_data, 'trie': trie})
+def add_sub_to_trie(sub,id,offset):
+    tmp_trie = trie
+    for letter in sub:
+        if letter not in tmp_trie.keys():
+            tmp_trie[letter] = {}
+            tmp_trie[letter]['sentences_ids'] = [{'id':id,'offset':offset}]
+        else:
+            tmp_trie[letter]['sentences_ids'].append({'id':id,'offset':offset})
+        tmp_trie = tmp_trie[letter]
+
+def add_sentence_to_trie(sentence,id):
+    for index in range(len(sentence)+1):
+        add_sub_to_trie(sentence[index:],id,index)
+
+
 
 def init_data():
     data = get_data_from_file("data.txt")
     data += get_data_from_file("freedom.txt")
-    for index, sentence in enumerate(data):
-        add_sentence_to_trie(sentence.lower(), index)
-        add_data_to_sentences_data(sentence, index, "data.txt")
-    data_for_file = {"sentences": sentences_data, "trie": trie}
+    for index,sentence in enumerate(data):
+        add_sentence_to_trie(sentence.lower(),index)
+        add_sentence_to_data(sentence,index,"data.txt")
+    data_for_file= {"sentences":sentences,"trie":trie}
     with open("data.json", "w") as data_file:
-        json.dump(data_for_file, data_file)
-
+        json.dump(data_for_file ,data_file)
+    # print(data)
+    # print(trie)
 
 init_data()
--- online.py
+++ online.py
@@ -1,28 +1,21 @@
-#from search import find_sentences
 from search_functoins import find_sentences
 
-
-def normal_form_query(query):
-    without_unnecessary_spaces = ' '.join([x for x in query.split(' ') if x != ''])
-    only_letter_or_number = ''.join(e for e in without_unnecessary_spaces if e.isalnum() or e == ' ').lower()
-    return only_letter_or_number
-
-
-# def find_matches(query):
-#     normal_query = normal_form_query(query)
-#     find_sentences(normal_form_query(query))
-#     #optional_matches = find_sentences(normal_query)
-#     #return optional_matches
+def normal_form(query):
+  without_unnecessary_spaces = ' '.join([x for x in query.split(' ') if x != ''])
+  only_letter_or_number = ''.join(e for e in without_unnecessary_spaces if e.isalnum() or e == ' ').lower()
+  return only_letter_or_number
 
 
-def main():
-    user_input = input("enter an input for search.\n")
-    while True:
-        if '#' not in user_input:
-            user_input += input(user_input)
+def get_input():
+    query = input("Begin search - enter your text:\n")
+    while(True):
+        if '#' in query:
+            query = input("New search - enter your text:\n")
         else:
-            user_input = input("enter an input for search.\n")
-        find_sentences(normal_form_query(user_input))
+            normal_query=normal_form(query)
+            find_sentences(normal_query)
+            query +=input(query)
 
 
-main()
+
+get_input()
